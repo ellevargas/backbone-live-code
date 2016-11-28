8e1d2d7005daebcaccac5179d9bbef03e03556bc
Elle Vargas <vargas.elle@gmail.com>
2016-11-28 11:22:24 -0800

    got basic task functionality working. also first push.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import Backbone from 'backbone';
 
 var taskData = [
   {
@@ -13,6 +14,27 @@ var taskData = [
   }
 ];
 
+var TaskView = Backbone.View.extend({
+  initialize: function(options) {
+    this.task = options.task;
+  },
+
+  render: function() {
+    var html = '<li class="task">';
+    html += '<h2>' + this.task.title + '</h2>';
+    html += '<p>' + this.task.description + '</p>';
+    html += '</li>';
+    this.$el.html(html);
+    return this;
+  }
+});
+
 $(document).ready(function() {
-  $('#test-area').append($('<p>Hello World!</p>'));
+  var taskListElement = $('.task-list');
+  var cardList = []
+  taskData.forEach(function(task) {
+    var card = new TaskView({task: task});
+    cardList.push(card);
+    taskListElement.append(card.render().$el);
+  });
 });
